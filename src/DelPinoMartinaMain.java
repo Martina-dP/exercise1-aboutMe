93616480cf1f82f2bed07fbd0be9f3e39f1c1087
Martina del Pino <martudelpino@gmail.com>
2024-11-29 12:51:51 +0100

    arreglo mal escritura

--- src/DelPinoMartinaMain.java
+++ src/DelPinoMartinaMain.java
@@ -1,5 +1,74 @@
-public class Main {
+import java.util.List;
+import java.util.Scanner;
+
+public class DelPinoMartinaMain {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DelPinoMartinaMain program = new DelPinoMartinaMain();
+        program.inicio();
+    }
+    public void inicio() {
+        List<String> hobbies = List.of("Leer", "Programar", "Viajar");
+        List<String> foods = List.of("Asado", "Pasta", "Helado");
+        List<String> funFacts = List.of(
+                "Water might not be wet",
+                "Football teams wearing red kits play better",
+                "You can be heavily pregnant and not realise");
+
+        DelPinoMartinaPerfil martina = new DelPinoMartinaPerfil(
+                "martina",
+                "Entrenando mucho para correr una maraton en el 2025",
+                hobbies,
+                foods,
+                funFacts
+        );
+        menuOpcions(martina);
+    }
+
+    public static void menuOpcions(DelPinoMartinaPerfil delPinoMartinaPerfil){
+
+        int opcion ;
+        boolean end = false;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Menu AboutMe: " + delPinoMartinaPerfil.getName());
+
+        while (true) {
+
+            System.out.println("1 = Ver la historia");
+            System.out.println("2 =  Ver hobbies y foods");
+            System.out.println("3 =  Ver fun fact aleatorio");
+            System.out.println("4 = Salir");
+            System.out.println("seleccione una opcion: ");
+
+            opcion = input.nextInt();
+
+            switch (opcion) {
+                case 1: {
+                    System.out.println("Story " + delPinoMartinaPerfil.getStory());
+                    break;
+                }
+                case 2: {
+                    System.out.println("Favorites");
+                    System.out.println("Hobbies: " + String.join(", ", delPinoMartinaPerfil.getHobbies()));
+                    System.out.println("Foods: " + String.join(", ", delPinoMartinaPerfil.getFoods()));
+                    break;
+                }
+                case 3: {
+                    List<String> funFacts = delPinoMartinaPerfil.getFunFacts();
+                    String randomFunFact = funFacts.get((int) (Math.random() * funFacts.size()));
+                    System.out.println("Fun Fact: " + randomFunFact);
+                    break;
+                }
+                case 4: {
+                    System.out.println("saliendo...");
+                    end = true;
+                    return;
+                }
+                default: {
+                    System.out.println("Opción no válida. Por favor, elige 1, 2, 3 o 4.");
+                    break;
+                }
+            }
+        }
     }
 }
--- src/DelPinoMartinaPerfil.java
+++ src/DelPinoMartinaPerfil.java
@@ -1,6 +1,53 @@
-public class Perfil {
-    String name;
-    String story;
-    Enum hobbies;
+import java.util.List;
 
+public class DelPinoMartinaPerfil {
+    private String name;
+    private String story;
+    private List<String> hobbies;
+    private List<String> foods;
+    private List<String> funFacts;
+
+    //Creando un constructor para nada mas pasar los valores de cada atributo por parametros en main
+    public DelPinoMartinaPerfil(
+            String name,
+            String story,
+            List<String> hobbies,
+            List<String> foods,
+            List<String> funFacts) {
+        this.name = name;
+        this.story = story;
+        this.hobbies = hobbies;
+        this.foods = foods;
+        this.funFacts = funFacts;
+    }
+
+    //Genero un getter por cada uno de los atributos que quiero generar una lectura de valores.
+    public String getName() {
+        return name;
+    }
+    public String getStory() {
+        return story;
+    }
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+    public List<String> getFoods() {
+        return foods;
+    }
+    public List<String> getFunFacts() {
+        return funFacts;
+    }
+
+    //Formateo la informacion para facilitar lectura
+    // el ":\n" hace un salto de linea depues de cada atributo
+    @Override
+    public String toString() {
+        return "DelPinoMartinaPerfil{" +
+                "name='" + name + ":\n" +
+                "story='" + story + ":\n" +
+                "hobbies=" + (hobbies.isEmpty() ? "No tienes hobbies" : String.join(", ", hobbies)) + ":\n" +
+                "foods=" + (foods.isEmpty() ? "No tienes foods" : String.join(", ", foods)) + ":\n" +
+                "funFacts=" + (funFacts.isEmpty() ? "No tienes funFacts" : String.join(", ", funFacts)) +
+                '}';
+    }
 }
